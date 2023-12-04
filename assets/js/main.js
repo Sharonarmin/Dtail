87b4b16b97ed06471e1ab2b95e4d86ce66f7b947
sharon <sharonarmin84@gmail.com>
2023-12-04 12:25:38 +0530

    first commit

---
+++ assets/js/main.js
@@ -0,0 +1,36 @@
+// active link
+const links = document.querySelectorAll('.link');
+
+links.forEach(link => {
+    link.addEventListener('click', () => {
+        links.forEach(innerLink => innerLink.classList.remove('active'));
+        link.classList.add('activeLink');
+    });
+});
+
+
+// swiper slider
+const progressCircle = document.querySelector(".autoplay-progress svg");
+const progressContent = document.querySelector(".autoplay-progress span");
+var swiper = new Swiper(".mySwiper", {
+  spaceBetween: 30,
+  centeredSlides: true,
+  autoplay: {
+    delay: 5500,
+    disableOnInteraction: false
+  },
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true
+  },
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev"
+  },
+  on: {
+    autoplayTimeLeft(s, time, progress) {
+      progressCircle.style.setProperty("--progress", 1 - progress);
+      progressContent.textContent = `${Math.ceil(time / 1000)}s`;
+    }
+  }
+});
